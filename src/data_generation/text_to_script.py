72ee36b40969c40c775934380dadcd1be98370e9
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-10-27 16:13:34 +0900

    added data

---
+++ src/data_generation/text_to_script.py
@@ -0,0 +1,50 @@
+import os
+
+class TextToScript:
+    def __init__ (self, base_path, language_l):
+        self.base_path = base_path
+        self.language_l = language_l
+
+    def main(self):
+        # Create output folder
+        self.create_output_dir(self.base_path, ['script', 'pre_gen'])
+
+        for language in self.language_l:
+            input_path = '{0}/data/{1}/'.format(self.base_path, language)
+            output_path = '{0}/script/pre_gen/{1}/'.format(self.base_path, language)
+            os.makedirs(output_path, exist_ok=True)
+            files_l = os.listdir(input_path)
+            files_l = sorted(files_l)
+            if '.DS_Store' in files_l:
+                files_l.remove('.DS_Store')
+            for file in files_l:
+                self.create_script(input_path + file, output_path + file.replace('.txt', '.py'))
+
+    def create_output_dir(self, base_path, dir_l):
+        for dir in dir_l:
+            path = '{0}/{1}'.format(base_path, dir)
+            os.makedirs(path, exist_ok=True)
+            base_path = path 
+    
+    def create_script(self, Input, Output):
+        with open(Input, 'r') as f:
+            source_file_l = f.readlines()
+            source_file_l = list(map(lambda x: '#' + x, source_file_l))
+            source_file_l.append('\n\ndef ')
+
+        with open(Output, 'w') as f:
+            for line in source_file_l:
+                f.write(line)
+
+
+if __name__ == '__main__':
+    base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    language_l = []
+    while True:
+        language = input('Enter language: en, ja, zh, or press enter to exit:')
+        if language == '':
+            break
+        language_l.append(language)
+    tts = TextToScript(base_path, language_l)
+    tts.main()
+    
---
+++ src/translation/translate.py
@@ -0,0 +1,48 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv('DEEPL_API_KEY')
+
+class Translate:
+    def __init__(self, base_path, language):
+        self.base_path = base_path
+        self.language = language
+
+    def main(self):
+        input_path = '{0}/data/en/'.format(self.base_path)
+        output_path = '{0}/data/{1}/'.format(self.base_path, self.language.lower())
+        os.makedirs(output_path, exist_ok=True)
+
+        files_l = os.listdir(input_path)
+        files_l = sorted(files_l)
+        if '.DS_Store' in files_l:
+            files_l.remove('.DS_Store')
+
+        for file in files_l:
+            self.translate(input_path + file, output_path + file)
+
+    def translate(self, Input, Output):
+        with open(Input, 'r') as f:
+            self.source_file_l = f.read()
+        
+        url = 'https://api.deepl.com/v2/translate'
+        params = {
+            'auth_key': API_KEY,
+            'text': self.source_file_l,
+            'source_lang': 'EN',
+            'target_lang': self.language 
+        }
+
+        request = requests.post(url, data=params)
+        result = request.json()
+
+        with open(Output, 'w') as f:
+            f.write(result['translations'][0]['text'])
+
+if __name__ == '__main__':
+    base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    for language in ['JA', 'ZH']:
+        translate = Translate(base_path, language)
+        translate.main()
